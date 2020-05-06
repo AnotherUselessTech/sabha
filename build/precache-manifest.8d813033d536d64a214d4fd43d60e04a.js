c033920f5de8f079d1d776bac1212a6ef9c6a8b6
Saiteja-vadlapatla <saitejavadlapatla@gmail.com>
2020-05-06 16:12:41 +0530

    adding build folder

---
+++ build/precache-manifest.8d813033d536d64a214d4fd43d60e04a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80d4f28e1ea9ec50c6d4faa2c55323ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6792f8167dbe5cfe696c",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8e1f66a976f68b3f63e5",
+    "url": "/static/js/2.72adb32f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.72adb32f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6792f8167dbe5cfe696c",
+    "url": "/static/js/main.4e2fd94e.chunk.js"
+  },
+  {
+    "revision": "ab7bee3a0877f74c797d",
+    "url": "/static/js/runtime-main.a0d63b0c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
